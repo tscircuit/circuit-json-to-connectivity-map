b94fe8f395bde8b43ad5355c4b1c7e20e21a3417
seveibar <seveibar@gmail.com>
2024-09-09 21:41:31 -0700

    fix basic test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,16 +1,35 @@
-import { test, expect } from "bun:test";
+import { test, expect } from "bun:test"
+import { findConnectedNetworks } from "../src"
 
 test("should create basic connectivity map", () => {
-	// Example usage
-	const connections: Array<NodeId[]> = [
-		["A", "B"],
-		["B", "C"],
-		["D", "E"],
-		["F", "G", "H"],
-		["I"],
-		["G", "J"],
-	];
+  // Example usage
+  const connections: Array<string[]> = [
+    ["A", "B"],
+    ["B", "C"],
+    ["D", "E"],
+    ["F", "G", "H"],
+    ["I"],
+    ["G", "J"],
+  ]
 
-	const result = findConnectedNetworks(connections);
-	console.log(result);
-});
+  const result = findConnectedNetworks(connections)
+
+  expect(result).toEqual([
+    {
+      netId: "net_0",
+      connectedNodeIds: ["A", "B", "C"],
+    },
+    {
+      netId: "net_3",
+      connectedNodeIds: ["D", "E"],
+    },
+    {
+      netId: "net_5",
+      connectedNodeIds: ["F", "G", "H", "J"],
+    },
+    {
+      netId: "net_8",
+      connectedNodeIds: ["I"],
+    },
+  ])
+})
