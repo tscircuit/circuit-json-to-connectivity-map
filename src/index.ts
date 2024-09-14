65aa12585c7311aa414026f17e70dc742b485f01
seveibar <seveibar@gmail.com>
2024-09-13 20:51:13 -0700

    add connecitivty map export and biome

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { findConnectedNetworks } from "./findConnectedNetworks"
 export { getSourcePortConnectivityMapFromCircuitJson } from "./getSourcePortConnectivityMapFromCircuitJson"
 export { getFullConnectivityMapFromCircuitJson } from "./getFullConnectivityMapFromCircuitJson"
+export { ConnectivityMap } from "./ConnectivityMap"
