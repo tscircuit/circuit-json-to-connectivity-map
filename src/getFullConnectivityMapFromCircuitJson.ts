6e03f0f25e609f222b05261e8d7a9435386872b6
Shibo <shibosoftwaredev@gmail.com>
2025-05-11 18:58:16 +0200

    better types

--- src/getFullConnectivityMapFromCircuitJson.ts
+++ src/getFullConnectivityMapFromCircuitJson.ts
@@ -1,4 +1,8 @@
-import type { AnyCircuitElement } from "circuit-json"
+import type {
+  AnyCircuitElement,
+  PcbTraceRoutePoint,
+  PcbTraceRoutePointWire,
+} from "circuit-json"
 import { findConnectedNetworks } from "./findConnectedNetworks"
 import { ConnectivityMap } from "./ConnectivityMap"
 
@@ -32,16 +36,19 @@ export const getFullConnectivityMapFromCircuitJson = (
         connections.push([pcb_plated_hole_id, pcb_port_id])
       }
     } else if (element.type === "pcb_trace") {
-      const { pcb_trace_id, source_trace_id, route } = element
+      const { pcb_trace_id, source_trace_id } = element
+      const route = Array.isArray(element.route)
+        ? element.route.filter((rp) => rp && rp.route_type === "wire")
+        : []
       if (source_trace_id && pcb_trace_id) {
         connections.push([pcb_trace_id, source_trace_id])
       }
       if (Array.isArray(route)) {
-        const startId = (
-          route.find((rp) => rp && (rp as any).start_pcb_port_id) as any
+        const startId = route.find(
+          (rp) => rp && rp.start_pcb_port_id,
         )?.start_pcb_port_id
-        const endId = (
-          route.find((rp) => rp && (rp as any).end_pcb_port_id) as any
+        const endId = route.find(
+          (rp) => rp && rp.end_pcb_port_id,
         )?.end_pcb_port_id
         if (startId && pcb_trace_id) {
           connections.push([startId, pcb_trace_id])
