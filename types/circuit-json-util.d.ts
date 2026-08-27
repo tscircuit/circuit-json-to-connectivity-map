f1ceea014f0fb6e8f0d77a660835839872238381
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-08-27 06:48:27 +0200

    chore: use circuit-json-util preview package

--- types/circuit-json-util.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@tscircuit/circuit-json-util/compute-gap-between-copper" {
-  type CircuitElement = import("circuit-json").AnyCircuitElement
-
-  export const computeGapBetweenCopper: (
-    first: CircuitElement,
-    second: CircuitElement,
-  ) => number
-}
